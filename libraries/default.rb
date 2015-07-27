fb4cab530c14e506c82f559f918cc623f2b38b31
Mike Stankavich <mike@kwyk.net>
2015-07-27 10:23:11 -0400

    Add explicit creds for AWS keys

--- libraries/default.rb
+++ libraries/default.rb
@@ -7,11 +7,9 @@ module AwsVpcNatInstance
       begin
         require 'aws-sdk'
 
-        aws_region = node['ec2']['placement_availability_zone'][0..-2]
-
-        Aws.config(access_key_id: node['aws-vpc-nat-instance']['access_key_id'],
-                   secret_access_key: node['aws-vpc-nat-instance']['secret_access_key'],
-                   region: aws_region)
+        Aws.config[:credentials] = Aws::Credentials.new(
+            node['aws-vpc-nat-instance']['access_key_id'],
+            node['aws-vpc-nat-instance']['secret_access_key'])
 
       rescue LoadError
         Chef::Log.error("Missing gem 'aws-sdk'. Use the default aws recipe to install it first.")
